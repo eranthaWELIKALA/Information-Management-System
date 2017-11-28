e08bbfab1325dcebe82cc1705a6caa20d5404dd4
Erantha Yasith Welikala <33684206+eranthaWELIKALA@users.noreply.github.com>
2017-11-29 02:03:02 +0530

    Update signup.php

--- signup.php
+++ signup.php
@@ -4,8 +4,12 @@ if(!isset($_POST["memID"]) || !isset($_POST["password"])){
 }
 $memIDStatus=true;
 require 'connect.php';
+
 $memID = mysqli_real_escape_string($connect, $_POST['memID']);
+$password = mysqli_real_escape_string($connect, $_POST['password']);
+
 $signup_query="SELECT * FROM members_login_details WHERE MembershipID='{$memID}'";
+
 if($is_signup_query_run=mysqli_query($connect,$signup_query)){
 			if(mysqli_num_rows($is_signup_query_run) == 1){
 				$memIDStatus=false;
@@ -21,13 +25,58 @@ if($is_signup_query_run=mysqli_query($connect,$signup_query)){
 					</div>';
 			}
 }
+if(isset($_POST["signup_submit"])){
+	$memID = mysqli_real_escape_string($connect, $_POST['memID']);
+	$password = mysqli_real_escape_string($connect, $_POST['password']);
+
+	$signup_query="SELECT * FROM members_login_details WHERE MembershipID='{$memID}'";
+
+	if($is_signup_query_run=mysqli_query($connect,$signup_query)){
+				if(mysqli_num_rows($is_signup_query_run) == 1){
+					$memIDStatus=false;
+					echo '<div class="container" align="center">
+							<div class="row">
+								<div class="col-md-3"></div>
+								<div class="col-md-6">
+									<div class="alert alert-info alert-dismissable">
+									<strong>Account for this Membership Id, is already created.</strong></div><br>
+								</div>
+								<div class="col-md-3"></div>
+							</div>
+						</div>';
+				}else{
+					require 'connect.php';
+					$memID = mysqli_real_escape_string($connect, $_POST['memID']);
+					$firstname = mysqli_real_escape_string($connect, $_POST['firstname']);
+					$lastname = mysqli_real_escape_string($connect, $_POST['lastname']);
+					$accepted = mysqli_real_escape_string($connect, 'false');
+					$request_query="INSERT INTO signup_requests (MembershipID, Firstname, Lastname, Password, Accepted) VALUES ('$memID', '$firstname', '$lastname', '$password',false)";
+					$is_request_query_run=mysqli_query($connect,$request_query);
+					if(!$is_request_query_run){
+							echo '<div class="container">
+										<div class="row">
+											<div class="col-md-4"></div>
+											<div class="col-md-4">
+												<div class="alert alert-danger alert-dismissable">
+												<a href="signup.php" class="close" data-dismiss="alert" arial-label="close">&times</a>
+												<strong>Your signup request isn\'t sent successfully!</strong></div><br>
+											</div>
+											<div class="col-md-4"></div>
+										</div>
+									</div>';		
+					}
+					header ("location:home.php");
+				}
+	}
+	
+}
 ?>
 <html>
 <head><title>
 <?php
 echo "SIGNUP ".$_POST['memID'];
 ?></title><link href="bootstrap/css/bootstrap.min.css" rel="stylesheet"></head>
-<form>
+<form action="signup.php" method="post">
 <p align="center">
 <table><div class="container"><div class="row">
 <div class="col-md-6" align="right">First Name </div>
@@ -41,8 +90,7 @@ echo "SIGNUP ".$_POST['memID'];
 <div class="row"><div class="col-md-6" align="right">Re-enter Password </div>
 <div class="col-md-6" align="right"><input class="form-control" type="password" name="rpassword"></div></div>
 <div class="row"><div class="col-md-6" align="right"></div>
-<div class="col-md-6" align="right"><input class="form-control" type="submit" value="SIGNUP" name="submit"></div></div></div>
+<div class="col-md-6" align="right"><input class="form-control" type="submit" value="SIGNUP" name="signup_submit"></div></div></div>
 </table></p>
 </form>
-
 </html>

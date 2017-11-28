88a29af228a1b87d5f656b0b0e0697f947015187
Erantha Yasith Welikala <33684206+eranthaWELIKALA@users.noreply.github.com>
2017-11-29 02:03:50 +0530

    Update connect.php

--- connect.php
+++ connect.php
@@ -3,11 +3,11 @@
  $mysql_user='root';
  $mysql_password='';
  
- if($connect=mysqli_connect($mysql_host,$mysql_user,$mysql_password)){
+ if($connect=mysqli_connect($mysql_host,$mysql_user,$mysql_password,'members')){
 	//echo "mySQL Connection Successful<br>";
-	if(@mysqli_select_db($connect,'members')){
-		//echo "database connected<br>";
-	}else die('database not connected<br>');
+	/*if(@mysqli_select_db($connect,'members')){
+		echo "database connected<br>";
+	}else die('database not connected<br>');*/
  }else die("Connection Unsuccessful<br>");
  
 ?>

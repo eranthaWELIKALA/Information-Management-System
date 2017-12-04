1ba0e1039ee71fb08eb8100809b8916be867fa21
Erantha Yasith Welikala <33684206+eranthaWELIKALA@users.noreply.github.com>
2017-12-04 15:52:26 +0530

    Update login.php

--- login.php
+++ login.php
@@ -1,44 +1,93 @@
-<?php
+<!DOCTYPE html><?php
 session_start();session_destroy();
 ?>
 
 <html>
-<head><title>LOGIN</title>
-<link href="bootstrap.min.css" rel="stylesheet">
-<script src="bootstrap.min.js"></script>
+<head>
+	<title>LOGIN</title>
+	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+	
+	<style>
+		body{
+			width:100%;
+			height:100%;
+			background-image:url('background2.jpg');
+			background-size:cover;
+			padding: 15px;
+		}
+		#one{
+			width:100%;
+			height:100%;
+			background-image:url('navbar.jpg');
+			background-size:cover;
+		}
+		.nav-pills > li.active > a, .nav-pills > li.active > a:focus {
+			color: black;
+			background-color: #fcd900;
+		}
+
+        .nav-pills > li.active > a:hover {
+            background-color: #000000;
+            color:white;
+        }
+	</style>
 </head>
+
 <body>
-<div class="container">
-	<div class="row"><div class="col-md-10"></div><div class="col-md-2">
-	<ul class="nav nav-pills">
-		<li><a href="home.php">Home</a></li>
-		<li class="active"><a>Login</a></li>
-	</ul>
-	</div></div>
-</div>
-<fieldset>
-<div class="container" align="center" >
-	<div style="width:196;height:196;border:2px solid #33b2ff;background-color:Gray;border-radius: 25px;">
-	<img src="login.png" class="img-circle" alt="Cinque Terre" width="176" height="176"> 
-	</div>
-</div>
-</fieldset>
 <br>
-<form action="login.php" method="post"><p align="center">
-<table>
-<tr>
-<td>Membership ID : </td>
-<td><input class="form-control" type="text" name="memID" placeholder="ex: 123" required></td>
-</tr>
-<tr>
-<td>Password : </td>
-<td><input class="form-control" type="password" name="password" placeholder="ex: ****" required></td>
-<td><input class="form-control" type="submit" value="LOGIN" name="submit"></td>
-</tr></table><table>
-</p>
-</form>
+<!--navigation bar-->
+<div class="container" id="one">
+	<br>
+	<div class="row">
+		<div class="col-md-2"></div>
+		<div class="col-md-8" align="center"><h2><font color="#ffd700">Old Boy Association - Login</font></h2></div>
+		<div class="col-md-2">
+			<ul class="nav nav-pills">
+				<li><a href="home.php"><span class="glyphicon glyphicon-home" style="color:#ffd900"></span><font color="#ffd900"> Home</font></a></li>
+				<li class="active"><a><span class="glyphicon glyphicon-user"></span> Login</a></li>
+			</ul>
+		</div>
+	</div>
+	<br>
+</div>
+
+<br><br>
+
+<div class="container">
+	<!--login logo-->
+	<fieldset>
+	<div class="container" align="center" >
+		<div style="width:196px;height:196px;border:2px solid #33b2ff;background-color:black;border-radius: 25px;">
+		<img src="login.png" class="img-circle" alt="Cinque Terre" width="176" height="176"> 
+		</div>
+	</div>
+	</fieldset>
+
+	<br><br>
+
+	<!--login form area-->
+	<form action="login.php" method="post">
+		<div class="row">
+			<div class="col-md-4" align="right"><font color="#ffd700">Membership ID : </font></div>
+			<div class="col-md-4"><input class="form-control" type="text" name="memID" placeholder="ex: 123" required></div>
+		</div>
+		<div class="row" align="center">
+			<div class="col-md-4" align="right"><font color="#ffd700">Password : </font></div>
+			<div class="col-md-4"><input class="form-control" type="password" name="password" placeholder="ex: ****" required></div>
+		</div><h6></h6>
+		<div class="row">
+			<div class="col-md-4"></div>
+			<div class="col-md-4"><input class="form-control" style="background-color:gold;color:black;" type="submit" value="LOGIN" name="submit"></div>
+		</div>
+	</form>
+</div>
+
 </body>
 </html>
+
 <?php
 if(isset($_POST["submit"])){
 	$error =array();
@@ -61,6 +110,9 @@ if(isset($_POST["submit"])){
 				//$row =  mysqli_fetch_assoc($is_query_run);
 				if($_SESSION['memID']=="reg000"){
 					header ("location:reg.php");
+				}
+				else if($_SESSION['memID']=="sec000"){
+					header ("location:sec.php");
 				}
 				else{
 					header ("location:user.php");
